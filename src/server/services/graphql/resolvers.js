15eef53ae98a59d40ff00d25a62b265b62cfd84b
Vincenzo <vnktux@protonmail.com>
2022-04-28 22:31:51 +0200

    Add Sequelize and Docker compose for test database

--- src/server/services/graphql/resolvers.js
+++ src/server/services/graphql/resolvers.js
@@ -1,3 +1,5 @@
+import logger from '../../helpers/logger';
+
 let posts = [{
     id: 2,
     text: 'Lorem ipsum',
@@ -19,6 +21,18 @@ const resolvers = {
     RootQuery: {
         posts(root, args, context){
             return posts;
+        },
+    },
+    RootMutation: {
+        addPost(root, { post, user}, context){
+            const postObject = {
+                ...post,
+                user,
+                id: posts.length + 1,
+            };
+            posts.push(postObject);
+            logger.log({ level: 'info', message: 'Post was created' });
+            return postObject;
         },
     },
 };
